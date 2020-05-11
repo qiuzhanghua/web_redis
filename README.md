a1c308934f7afea2f753ab31ccc7ec27f1173129
qiuzhanghua <qiuzhanghua@icloud.com>
2020-05-11 12:00:27 +0800

    0.2.0 try bb8

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ cargo run
 REDIS_URL="redis://localhost/0" cargo run
 ```
 
-### perf
+### perf(r2d2 + redis)
 ```text
 ➜  ~ wrk -t12 -c200 -d30s http://localhost:8080/
 Running 30s test @ http://localhost:8080/
@@ -19,3 +19,19 @@ Running 30s test @ http://localhost:8080/
 Requests/sec:  16845.26
 Transfer/sec:      1.94MB
 ```
+### perf(bb8 + redis + async)
+
+```text
+➜  src git:(master) ✗ wrk -t8 -c200 -d30s http://localhost:8080/
+Running 30s test @ http://localhost:8080/
+  8 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     1.11ms   79.74us  10.74ms   96.37%
+    Req/Sec     3.21k   661.66     5.94k    84.51%
+  672473 requests in 30.10s, 77.60MB read
+  Socket errors: connect 0, read 57, write 0, timeout 0
+Requests/sec:  22340.66
+Transfer/sec:      2.58MB
+```
+
+(Old Mac)
--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,59 @@
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate serde_json;
-#[macro_use]
 extern crate actix_web;
+extern crate log;
+extern crate serde_json;
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-use r2d2::{Pool, PooledConnection};
+use actix_web::{web, App, HttpRequest, HttpServer};
+use bb8_redis::{bb8, RedisConnectionManager, RedisPool};
+
+pub const REDIS_POOL_SIZE: u32 = 100;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
     env_logger::init();
-
-    HttpServer::new(move || App::new().service(web::resource("/").route(web::get().to(index))))
-        .keep_alive(actix_http::KeepAlive::Os)
-        // .shutdown_timeout(30)
-        // .maxconn(50000)
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
-}
-
-pub async fn index(_request: HttpRequest) -> impl Responder {
-    use r2d2_redis::redis::Commands;
-    use std::collections::HashMap;
-    let mut conn = get_redis_connection();
-    // conn.get::<&str, String>("key").unwrap()
-    let key = format!("{}:{}", "people", "834212ef-7022-459e-a281-16342addc1d0");
-    let map = conn.hgetall::<&str, HashMap<String, String>>(&key);
-    match map {
-        Ok(m) => m.get("name").unwrap().clone(),
-        _ => "Error".to_string(),
-    }
-//     "redis"
-}
-
-// pub fn establish_redis_connection() -> PooledConnection<r2d2_redis::RedisConnectionManager> {
-//     dotenv::dotenv().ok();
-//     let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
-//     let manager = r2d2_redis::RedisConnectionManager::new(redis_url).unwrap();
-//     let pool = r2d2::Pool::builder()
-//         .max_size(REDIS_POOL_SIZE)
-//         .build(manager)
-//         .unwrap();
-//     pool.get().unwrap()
-// }
-
-pub const REDIS_POOL_SIZE: u32 = 100;
-
-lazy_static! {
-    pub static ref REDIS_POOL: Pool<r2d2_redis::RedisConnectionManager> = {
-        dotenv::dotenv().ok();
-        let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
-        let manager = r2d2_redis::RedisConnectionManager::new(redis_url).unwrap();
-        r2d2::Pool::builder()
+    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
+    let manager = RedisConnectionManager::new(redis_url).unwrap();
+    let pool = RedisPool::new(
+        bb8::Pool::builder()
             .max_size(REDIS_POOL_SIZE)
             .build(manager)
-            .expect("Failed to create redis pool.")
-    };
+            .await
+            .unwrap(),
+    );
 
-    // Used to update core data into redis master, such as person, role and dept etc.
-    // pub static ref MASTER_REDIS_POOL: Pool<r2d2_redis::RedisConnectionManager> = {
-    //     dotenv().ok();
-    //     let redis_url = env::var("MASTER_REDIS_URL").expect("MASTER_REDIS_URL must be set");
-    //     let manager = r2d2_redis::RedisConnectionManager::new(redis_url).unwrap();
-    //     r2d2::Pool::builder()
-    //         .max_size(REDIS_POOL_SIZE)
-    //         .build(manager)
-    //         .expect("Failed to create master redis pool.")
-    // };
+    // let p = pool.clone();
+    // let mut conn = p.get().await.unwrap();
+    // let conn = conn.as_mut().unwrap();
+    // let reply: String = cmd("PING").query_async(conn).await.unwrap();
+    // assert_eq!("PONG", reply);
 
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .service(web::resource("/").route(web::get().to(index)))
+    })
+    .keep_alive(actix_http::KeepAlive::Os)
+    // .shutdown_timeout(30)
+    // .maxconn(50000)
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
 
-pub fn get_redis_connection() -> PooledConnection<r2d2_redis::RedisConnectionManager> {
-    REDIS_POOL.clone().get().unwrap()
+pub async fn index(_request: HttpRequest, redis: web::Data<RedisPool>) -> std::io::Result<String> {
+    let mut conn = redis.get().await.unwrap();
+    let conn = conn.as_mut().unwrap();
+
+    //  let client = redis::Client::open("redis://localhost/0").unwrap();
+    //
+    // client.get_multiplexed_async_connection();
+    // let mut conn = client.get_async_connection().await.unwrap();
+    let result = redis::cmd("GET")
+        .arg(&["key"])
+        .query_async(conn)
+        .await
+        .unwrap();
+    Ok(result)
+    // // let x = conn.get::<&str, &str>("key").await.unwrap();
+    // Ok("redis".to_string())
 }
