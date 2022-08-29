c65c05f79ed347bcf41be1f385b8bba86a474e70
qiuzhanghua <qiuzhanghua@icloud.com>
2022-08-29 08:54:20 +0800

    upgrade && test under m1x

--- README.md
+++ README.md
@@ -35,3 +35,37 @@ Transfer/sec:      2.58MB
 ```
 
 (Old Mac)
+
+
+## perf(bb8 + redis + async)
+
+```text
+❯ wrk -t8 -c200 -d30s http://localhost:8080/
+Running 30s test @ http://localhost:8080/
+  8 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     5.18ms    2.17ms  33.88ms   71.33%
+    Req/Sec     4.86k   680.83     6.30k    64.17%
+  1160658 requests in 30.02s, 133.93MB read
+  Socket errors: connect 0, read 12, write 0, timeout 0
+Requests/sec:  38665.86
+Transfer/sec:      4.46MB
+```
+
+(m1x)
+
+## perf(bb8 + redis + async)
+with cargo build --release
+```text
+❯ wrk -t8 -c200 -d30s http://localhost:8080/
+Running 30s test @ http://localhost:8080/
+  8 threads and 200 connections
+  Thread Stats   Avg      Stdev     Max   +/- Stdev
+    Latency     3.28ms    2.08ms  37.20ms   83.74%
+    Req/Sec     8.21k     2.11k   12.54k    77.58%
+  1960987 requests in 30.02s, 226.29MB read
+Requests/sec:  65324.65
+Transfer/sec:      7.54MB
+```
+
+(m1x)
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ extern crate actix_web;
 extern crate log;
 extern crate serde_json;
 
+use crate::bb8::Pool;
 use actix_web::{web, App, HttpRequest, HttpServer};
-use bb8_redis::{bb8, RedisConnectionManager, RedisPool};
+use bb8_redis::{bb8, RedisConnectionManager};
 
 pub const REDIS_POOL_SIZE: u32 = 100;
 
@@ -13,18 +14,16 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
     let manager = RedisConnectionManager::new(redis_url).unwrap();
-    let pool = RedisPool::new(
-        bb8::Pool::builder()
-            .max_size(REDIS_POOL_SIZE)
-            .min_idle(Some(REDIS_POOL_SIZE - 2))
-            .build(manager)
-            .await
-            .unwrap(),
-    );
+    let pool: Pool<RedisConnectionManager> = bb8::Pool::builder()
+        .max_size(REDIS_POOL_SIZE)
+        .min_idle(Some(REDIS_POOL_SIZE - 2))
+        .build(manager)
+        .await
+        .unwrap();
 
     HttpServer::new(move || {
         App::new()
-            .data(pool.clone())
+            .app_data(web::Data::new(pool.clone()))
             .service(web::resource("/").route(web::get().to(index)))
     })
     .keep_alive(actix_http::KeepAlive::Os)
@@ -35,12 +34,15 @@ async fn main() -> std::io::Result<()> {
     .await
 }
 
-pub async fn index(_request: HttpRequest, redis: web::Data<RedisPool>) -> std::io::Result<String> {
+pub async fn index(
+    _request: HttpRequest,
+    redis: web::Data<Pool<RedisConnectionManager>>,
+) -> std::io::Result<String> {
     let mut conn = redis.get().await.unwrap();
-    let conn = conn.as_mut().unwrap();
+    // let conn = conn.as_mut().unwrap();
     let result = redis::cmd("GET")
-        .arg(&["key"])
-        .query_async(conn)
+        .arg(&["hello"])
+        .query_async(&mut *conn)
         .await
         .unwrap();
     Ok(result)
